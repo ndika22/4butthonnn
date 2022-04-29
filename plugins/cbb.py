3ba4fd881b23c2591f4ea1646155a373efd0ae72
oscarprojek <95014364+oscarprojek@users.noreply.github.com>
2022-04-29 18:35:06 +0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,10 +13,10 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n@{client.username} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.\n\n • Creator: @{OWNER}\n • Jasa Bot Murah: <a href='https://t.me/PoocongOnlen/182'>Disini</a>\n • Owner Repo: @pocongonlen\n\n👨‍💻 Maintened bot by @Pocongproject</b>\n",
+            text=f"<b>Tentang Bot ini:\n\n@{client.username} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.\n\n • Creator: @{OWNER}\n • Source Code: <a href='https://t.me/skyhot17'>File-Sharing</a>\n • Owner Repo: @abc8a\n\n👨‍💻 Maintened by @abc8a</b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Tutup", callback_data="close")]]
+                [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
             ),
         )
     elif data == "close":
